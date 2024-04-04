40f918f7e9432635118a42e545a59305d375401c
domakun <denglieli@canpoint.cn>
2024-04-04 17:06:00 +0800

    项目初始化

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,13 @@ import {createRouter, createWebHistory} from 'vue-router'
 // @ts-ignore
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'dressUp',
-            component: () => import('../views/DressUp.vue')
-        },
-    ]
+    // routes: [
+    //     {
+    //         path: '/',
+    //         name: 'dressUp',
+    //         component: () => import('../views/DressUp.vue')
+    //     },
+    // ]
 })
 
 export default router
